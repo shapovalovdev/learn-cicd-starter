9239e8cedd6c0daf1ac5642f9eab191356ba446a
Evgenii Shapovalov <eshapovalov.dev@gmail.com>
2026-03-14 10:22:02 +0100

    Fix security issues...

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +91,16 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:        ":" + port,
+		Handler:     router,
+		ReadTimeout: time.Second * 10,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	p, err := strconv.Atoi(port)
+	if err != nil || p < 1 || p > 65535 {
+		log.Fatal("invalid port")
+	}
+
+	log.Printf("Serving on port: %d", p)
 	log.Fatal(srv.ListenAndServe())
 }
